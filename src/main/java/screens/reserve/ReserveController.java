1c7d9d744371f9967d27db39219b97c44ca91138
Rheyvene Balogbog <balogbogrheyene@gmail.com>
2026-05-04 21:38:53 +0800

    ReserveController WIP

--- src/main/java/screens/reserve/ReserveController.java
+++ src/main/java/screens/reserve/ReserveController.java
@@ -1,4 +1,27 @@
 package screens.reserve;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import utilities.javafxRelated.NavigationUtils;
+
 public class ReserveController {
+    @FXML private TextField equipmentName;
+    @FXML private TextField purpose;
+    @FXML private TextField affiliation;
+    @FXML private TextField supervisor;
+    @FXML private Button back;
+    @FXML private Button reserve;
+
+
+    public void onBackButtonClicked(ActionEvent actionEvent) {
+        NavigationUtils.switchScene(actionEvent,"/screens/dashboard/MainView.fxml");
+    }
+
+    public void onReserveButtonClicked(ActionEvent actionEvent) {
+
+    }
 }
