4fddd4dd36ece8cfad6242dc8c49d3fd6af35c39
znox-x <pastor.khyllejosh@gmail.com>
2026-05-04 14:37:20 +0800

    refactored AuthService and LoginController with UserDAO

--- src/main/java/utilities/daoRelated/UserDAO.java
+++ src/main/java/utilities/daoRelated/UserDAO.java
@@ -1,4 +1,60 @@
 package utilities.daoRelated;
 
+import data.User;
+import org.mindrot.jbcrypt.BCrypt;
+import utilities.sqlRelated.MySqlConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAO {
+    // tried my best to adhere to single responsibility principle
+    private final String FIND_BY_ID = "SELECT * FROM users WHERE id = ?";
+    private final String FIND_BY_EMAIL = "SELECT * FROM users WHERE email = ?";
+
+    // finds user from database that matches the provided id
+    public User findUser(int id) {
+        try (Connection c = MySqlConnection.getConnection();
+             PreparedStatement statement = c.prepareStatement(FIND_BY_ID)) {
+
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                return mapUser(rs);
+            }
+        } catch (SQLException e) {
+            System.err.println("Database connection or query failed: " + e.getMessage());
+        }
+        return null;
+    }
+
+    // finds user from database that matches the provided email
+    public User findUser(String email) {
+        try (Connection c = MySqlConnection.getConnection();
+             PreparedStatement statement = c.prepareStatement(FIND_BY_EMAIL)) {
+
+            statement.setString(1, email);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                return mapUser(rs);
+            }
+        } catch (SQLException e) {
+            System.err.println("Database connection or query failed: " + e.getMessage());
+        }
+        return null;
+    }
+
+    // helper; makes the user class derived from the database
+    private User mapUser(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("email"),
+                rs.getString("password")
+        );
+    }
 }
--- src/main/java/utilities/sqlRelated/AuthService.java
+++ src/main/java/utilities/sqlRelated/AuthService.java
@@ -4,6 +4,7 @@ package utilities.sqlRelated;
 
 import org.mindrot.jbcrypt.BCrypt;
 import data.User;
+import utilities.daoRelated.UserDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,30 +13,17 @@ import java.sql.SQLException;
 
 public class AuthService {
 
-    public static User authenticate(String email, String password) {
-        String query = "SELECT * FROM users WHERE email = ?";
+    private final UserDAO userDAO = new UserDAO();
 
-        try (Connection c = MySqlConnection.getConnection();
-             PreparedStatement statement = c.prepareStatement(query)) {
+    // business logic; checks if given password matches in the database
+    public User login(String email, String password) {
+        User user = userDAO.findUser(email);
+        if (user == null)   return null;
 
-            statement.setString(1, email);
-            ResultSet rs = statement.executeQuery();
-
-            if (rs.next()) {
-                String dbPasswordHash = rs.getString("password");
-
-                if (BCrypt.checkpw(password, dbPasswordHash)) {
-                    return new User(
-                            rs.getInt("id"),
-                            rs.getString("name"),
-                            rs.getString("email"),
-                            dbPasswordHash
-                    );
-                }
-            }
-        } catch (SQLException e) {
-            System.err.println("Database connection or query failed: " + e.getMessage());
+        if (BCrypt.checkpw(password, user.getPassword())) {
+            return user;
         }
+
         return null;
     }
 }
