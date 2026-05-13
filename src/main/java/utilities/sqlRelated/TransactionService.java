30fd41f6db5a1d45e49c1f9772a7e8151f117bfc
znox-x <pastor.khyllejosh@gmail.com>
2026-05-13 16:48:56 +0800

    TransactionService class

---
+++ src/main/java/utilities/sqlRelated/TransactionService.java
@@ -0,0 +1,13 @@
+package utilities.sqlRelated;
+
+import data.Transaction;
+
+public class TransactionService {
+    public boolean processBorrow(Transaction t) {
+        return true;
+    }
+
+    public boolean processReturn(Transaction t) {
+        return true;
+    }
+}
